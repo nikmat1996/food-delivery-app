745897d840b6e09f2c5c7f488955b5468f31435d
nikmat1996 <nikhilmathew1996@gmail.com>
2020-06-14 10:18:38 +0530

    Updated Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,15 +7,15 @@ export default function Header(props) {
     const adminLoggedIn = false
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-            <Navbar.Brand><Link to="/">FOOD DELIVERY APP</Link></Navbar.Brand>
+            <Navbar.Brand><Link className="ml-5 nav-link text-secondary" to="/">FOOD DELIVERY APP</Link></Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="ml-auto">
-                {!adminLoggedIn? null :<Link className="mr-5" to="/allDetails">STATS</Link>}
-                {!adminLoggedIn? null :<Link className="mr-5" to="/add">ADD RESTAURANT</Link>}
-                {!userLoggedIn? null :<Link className="mr-5" to="/orders">ORDERS</Link>}
-                {adminLoggedIn? null :<Link className="mr-5" to="/cart">CART</Link>}
-                <Link className="mr-5" to="/log">{userLoggedIn || adminLoggedIn ?'LOGOUT' :'LOGIN'}</Link>
+                {!adminLoggedIn? null :<Link className="mr-5 nav-link" to="/allDetails">STATS</Link>}
+                {!adminLoggedIn? null :<Link className="mr-5 nav-link" to="/add">ADD RESTAURANT</Link>}
+                {!userLoggedIn? null :<Link className="mr-5 nav-link" to="/orders">ORDERS</Link>}
+                {adminLoggedIn? null :<Link className="mr-5 nav-link" to="/cart">CART</Link>}
+                <Link className="mr-5 nav-link" to="/log">{userLoggedIn || adminLoggedIn ?'LOGOUT' :'LOGIN'}</Link>
             </Nav>
             </Navbar.Collapse>
         </Navbar>
