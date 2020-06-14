cf38ac1d987f0fc46a3443ef6869ec63f11b992b
nikmat1996 <nikhilmathew1996@gmail.com>
2020-06-14 20:54:31 +0530

    added HomePage data in grid

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,27 +1,54 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { Carousel } from "react-bootstrap";
+import { Carousel, Card, ListGroup,
+         ListGroupItem, Button, Badge,
+         Container, Col, Row } from "react-bootstrap";
 
 function Home(props) {
     const { data, carouselData } = props
     return (
-        <div className="container mt-3 p-3"><Carousel>{carouselData.map(item => (
+        <Container>
             
-                <Carousel.Item key={item.p}>
-                    <img
-                    className="d-block w-100"
-                    src={item.img}
-                    alt="First slide"
-                    />
-                    <Carousel.Caption>
-                    <h3 className="text-white-50">{item.h3}</h3>
-                    <p className="text-secondary">{item.p}</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
+            <Carousel>
+                {carouselData.map(item => (
+                
+                    <Carousel.Item key={item.p}>
+                        <img
+                        className="d-block w-100"
+                        src={item.img}
+                        alt="fruits"
+                        />
+                        <Carousel.Caption>
+                        <h3 className="text-white-50">{item.h3}</h3>
+                        <p className="text-secondary">{item.p}</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
 
-            ))}
+                ))}
             </Carousel>
-        </div>
+
+            
+            <Row>
+                {data.map(item => (
+                <Col xs={12} md={6} lg={4} className="p-2 mr-0">
+                    <Card className="" style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+                    
+                        <ListGroup className="list-group-flush text-center">
+                        <ListGroupItem>{item.food_name.toUpperCase()}<Badge className="ml-2" variant="secondary">{item.rating}<i className="fas fa-star ml-1"></i></Badge></ListGroupItem>
+                        <ListGroupItem>Rs {item.price}</ListGroupItem>
+                    
+                        </ListGroup>
+                        <Card.Body>
+                        <Button className="mr-3 text-"variant="secondary">Add to Cart</Button>
+                        <Card.Link className="ml-auto"href="#">Show Restaurant</Card.Link>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            ))}
+            </Row>
+           
+        </Container>
 
     )
 }
